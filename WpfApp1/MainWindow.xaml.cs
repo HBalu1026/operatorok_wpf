6e50ecf416e2ae24a7d103e23723f7565b28140d
hernadi.balazs <hernadi.balazs@58-03>
2023-09-13 13:04:02 +0200

    Add project files.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+
+    class Beolvasas
+    {
+        public int op1, op2;
+        public string operato;
+
+        public Beolvasas(int op1, string operato, int op2)
+        {
+            this.op1 = op1;
+            this.operato = operato;
+            this.op2 = op2;
+        }
+    }
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void fajlBeolvasas(object sender, RoutedEventArgs e)
+        {
+            var tombHossz = 0;
+            var fajl = new OpenFileDialog();
+            if (fajl.ShowDialog() == true)
+            {
+                lbEleres.Content = fajl.FileName;
+
+            }
+            string eleresiUt = fajl.FileName;
+            string[] beolvasott = File.ReadAllLines(eleresiUt, Encoding.UTF8);
+            List<Beolvasas> beolvasottTomb = new List<Beolvasas>();
+            for (int i = 0; i < beolvasott.Length; i++)
+            {
+                string[] splitelt = beolvasott[i].Split(' ');
+                beolvasottTomb.Add(new Beolvasas(Convert.ToInt32(splitelt[0]), (splitelt[1]), Convert.ToInt32(splitelt[2])));
+                tombHossz++;
+            }
+
+            //2
+            lbMasodikFeladat.Content = tombHossz.ToString();
+
+            //3
+
+
+        }
+        
+        
+    }
+}
+  
---
+++ WpfApp1/beolvasas.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    internal class beolvasas
+    {
+        int op1, op2;
+        string operato;
+
+        public beolvasas(int op1, int op2, string operato)
+        {
+            this.op1 = op1;
+            this.op2 = op2;
+            this.operato = operato;
+        }
+
+        /* old meg az alábbi feladatsort wpf-ben! A fájlok beolvasásakor hozz létre egyúj osztályt és read all line-t használj!
+           A következő feladatban kifejezéseket tartalmazó szöveges állományból kell adatokat kinyernie és feldolgoznia.
+A megoldás során vegye figyelembe a következőket:
+'A képernyőre Írást igénylő részfeladatok eredményének megjelenĺtése előtt Írja a képernyőre a feladat sorszámát (például: 3. feladat:)!
+'Az egyes feladatokban a kiírásokat a minta szerint készítse el!
+•Az ékezetmentes azonosítók és kiírások is elfogadottak.
+'Az azonosítókat kis- és nagybetűkkel is kezdheti.
+•A program megírásakor az állományban lévő adatok helyes szerkezetét nem kell ellenőriznie, feltételezheti, hogy a rendelkezésre álló adatok a leírtaknak megfelelnek.
+'Megoldását úgy készítse el, hogy az azonos szerkezetú, de tetszőleges bemeneti adatok mellett is helyes eredményt adjon!
+A kifej ezesek. txt forrásállomány soraiban aritmetikai kifejezések találhatók a következő leírás szerint:
+• Soronként az első adat a kifejezés első operandusa. Feltételezheti, hogy egész számra alakítható.
+KORLÁTOZOTT TERJESZTÉSŰ!		1. sz. példány
+'A második adat szöveges típusú, maximum három karakter hosszúságú. Az operátort határozza meg! Lehet olyan eset, hogy az operátor nem értelmezhető aritmetikai operátorként.
+•A harmadik adat a kifejezés második operandusa. Feltételezheti, hogy egész számra alakítható.
+'Az adatokat szóközökkel választottuk el, például:
+500 mod 265
+780 / 114
+814 div 200
+470 - 531
+802 * 553
+76€ 767
+
+Készítsen konzolos alkalmazást (projektet) a következő feladatok megoldásához, melynek projektjét Operátorok néven mentse el!
+1.	Olvassa be a kifej ezesek . txt állomány sorait és tárolja az adatokat egy olyan összetett adatszerkezetben (pl. vektor, lista stb.), amely használatával a további feladatok megoldhatók!
+2.	Határozza meg és írja ki a képernyőre, hogy az állomány hány kifejezést tartalmaz!
+3.	A maradékos osztás operátorát a mod szóval jelöltük az állományban. Határozza meg és Írja a képernyőre a maradékos osztást tartalmazó kifejezések számát!
+    051C 4. Döntse el, hogy a forrásállományban található-e olyan kifejezés, amelyben mindkét operandus maradék nélkül osztható tízzel! Az eldöntés eredményét Írja a képernyőre! A keresést ne folytassa, ha a választ meg tudja adni!
+5.	Az egész osztás operátorát a div szóval jelöltük az állományban. Készítsen statisztikát az összeadás (+), kivonás (—), szorzás (*), valós osztás (/), egész osztás (div) és maradékos osztás (mod) operátorokat tartalmazó kifejezések számáról!
+6.	Készítsen szöveges típusú adattal visszatérő függvényt, metódust vagy jellemzőt a kifejezés éltékének meghatározására! A függvény az előző feladatban felsorolt operátorokat tudja kezelni, ismeretlen operátor esetén térjen vissza a „Hibás operátor!” üzenettel! Helyes operátor esetén sem lehet egy kifejezés értékét mindig meghatározni (pl. túlcsordulás, nullával való osztás stb.), ilyen esetben a függvény térjen vissza az „Egyéb hiba!” üzenettel!
+7.	Kérjen be a felhasználótól egy kifejezést, amiről feltételezheti, hogy a forrás  állományban lévő kifejezések leírásának megfelel! Határozza meg az előző feladatban definiált mggvény felhasználásával a kifejezés értékét, majd írja ki azt a minták szerint! A feladatot ismételje a „vége” inputig!
+8.	Készítsen szöveges állományt eredmenyek . txt néven a minta szerint, melyben meghatározza a forrásállományban lévő kifejezések eredményeit!
+*/
+    }
+}
